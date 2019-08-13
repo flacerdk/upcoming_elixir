28b1f67679073f0a9d7aeef2e75850d5e504177f
Felipe Lacerda <fegolac@gmail.com>
2019-08-13 21:58:07 +0200

    :sparkles: Extend get_venues to get all venues
    Also, run mix format.

--- lib/upcoming_elixir.ex
+++ lib/upcoming_elixir.ex
@@ -13,41 +13,75 @@ defmodule Upcoming do
 
   """
 
-  def venues do
-    [4179319, 4179234, 3756504, 54162, 811381, 44268, 34460, 2740208, 4179379, 3723574, 1500938, 32939]
+  def copenhagen_venues do
+    [
+      4_179_319,
+      4_179_234,
+      3_756_504,
+      54162,
+      811_381,
+      44268,
+      34460,
+      2_740_208,
+      4_179_379,
+      3_723_574,
+      1_500_938,
+      32939
+    ]
   end
 
   def base_url do
     api_key = Application.get_env(:upcoming_elixir, :APIKey)
+
     URI.parse("https://api.songkick.com/api/3.0")
     |> Map.put(:query, URI.encode_query(%{"apikey" => api_key}))
   end
 
-  def append_url(relative_url) do
-    %{:path => base_path, :query => base_query} = base_url()
+  def append_url(relative_url, base \\ base_url()) do
+    %{:path => base_path, :query => base_query} = base
     %{:path => additional_path, :query => additional_query} = URI.parse(relative_url)
-    query = Map.merge(
-      URI.decode_query(base_query || ""),
-      URI.decode_query(additional_query || "")
-    )
-    |> URI.encode_query
+
+    query =
+      Map.merge(
+        URI.decode_query(base_query || ""),
+        URI.decode_query(additional_query || "")
+      )
+      |> URI.encode_query()
 
     base_url()
-    |> Map.put(:path, base_path <> additional_path)
+    |> Map.put(:path, base_path <> (additional_path || ""))
     |> Map.put(:query, query)
   end
 
-  def fetch(relative_url) do
-    {:ok, response} = append_url(relative_url) |> Mojito.get
-    %{"resultsPage" => %{"results" => results}} = Poison.decode!(response.body)
-    results
+  def fetch(relative_url, page \\ 1, per_page \\ 50) do
+    {:ok, response} =
+      append_url("?page=#{page}&per_page=#{per_page}", append_url(relative_url)) |> Mojito.get()
+
+    %{"resultsPage" => %{"results" => results, "totalEntries" => total_entries}} =
+      Poison.decode!(response.body)
+
+    %{:results => results, :max => total_entries}
   end
 
   def get_venue_calendar(venue_id) do
-    fetch("/venues/#{venue_id}/calendar.json") |> Map.get("event")
+    %{:results => %{"event" => events}} = fetch("/venues/#{venue_id}/calendar.json")
+    events
   end
 
-  def get_venues(location) do
-    fetch("/search/venues.json?query=#{location}") |> Map.get("venue")
+  def get_venues(location, page \\ 1, per_page \\ 50) do
+    %{:results => results, :max => max} =
+      fetch("/search/venues.json?query=#{location}", page, per_page)
+
+    case results do
+      %{"venue" => venues} ->
+        if (page - 1) * length(venues) >= max do
+          venues
+        else
+          venues ++ get_venues(location, page + 1, per_page)
+        end
+
+      _ ->
+        []
+    end
   end
 end
