fa43f58626c07faab737df189c4b7fb37462d39e
Felipe Lacerda <fegolac@gmail.com>
2019-08-18 22:00:54 +0200

    :bug: Event.parse: cover performance=null

--- lib/event.ex
+++ lib/event.ex
@@ -1,12 +1,19 @@
 defmodule Event do
+  @derive {Jason.Encoder, only: [:artist, :date]}
   defstruct [:artist, :date]
 
   def parse(%{
-        "performance" => [%{"artist" => %{"displayName" => name}} | _],
+        "performance" => performance,
         "start" => %{"date" => date}
       }) do
+    artist =
+      case performance do
+        [%{"artist" => %{"displayName" => name}} | _] -> name
+        _ -> nil
+      end
+
     %Event{
-      artist: name,
+      artist: artist,
       date:
         case Date.from_iso8601(date) do
           {:ok, dt} -> dt
