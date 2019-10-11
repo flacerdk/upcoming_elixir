938b32d50e3c96e05596cb8292b4944e730dda59
Felipe Lacerda <fegolac@gmail.com>
2019-10-11 20:29:27 +0200

    venue_controller: Render events from calendar

--- lib/upcoming_elixir_web/controllers/venue_controller.ex
+++ lib/upcoming_elixir_web/controllers/venue_controller.ex
@@ -16,7 +16,8 @@ defmodule UpcomingWeb.VenueController do
     if venue == nil do
       conn |> put_status(:not_found) |> put_view(UpcomingWeb.ErrorView) |> render("404.html")
     else
-      render(conn, "show.html", venue: venue)
+      events = Upcoming.Venue.get_calendar(venue)
+      render(conn, "show.html", events: events)
     end
   end
 end
