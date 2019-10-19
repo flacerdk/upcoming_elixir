68942f82f1ecc880b53a67a784a6583e8d7a25b7
Felipe Lacerda <fegolac@gmail.com>
2019-10-19 17:06:40 +0200

    Add function to fetch all of a location's events

--- lib/upcoming_elixir/location.ex
+++ lib/upcoming_elixir/location.ex
@@ -15,4 +15,18 @@ defmodule Upcoming.Location do
     |> cast(attrs, [:name])
     |> validate_required([:name])
   end
+
+  def events(location) do
+    import Ecto.Query, only: [from: 2]
+
+    from(l in Upcoming.Location,
+      where: l.id == ^location.id,
+      left_join: venues in assoc(l, :venues),
+      left_join: events in assoc(venues, :events),
+      preload: [venues: {venues, events: events}]
+    )
+    |> Upcoming.Repo.one()
+    |> Map.get(:venues)
+    |> Enum.flat_map(fn %Upcoming.Venue{events: events} -> events end)
+  end
 end
