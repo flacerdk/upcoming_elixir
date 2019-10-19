3b9d94bf249d0562f43d605dbf70d25e4a896acf
Felipe Lacerda <fegolac@gmail.com>
2019-10-19 15:54:01 +0200

    Add songkick_id to event

--- lib/upcoming_elixir/event.ex
+++ lib/upcoming_elixir/event.ex
@@ -4,16 +4,19 @@ defmodule Upcoming.Event do
   schema "events" do
     field :artist, :string
     field :date, :date, null: true
+    field :songkick_id, :string
     belongs_to(:venue, Upcoming.Venue)
   end
 
   def changeset(event, params \\ %{}) do
     event
-    |> Ecto.Changeset.cast(params, [:artist, :date, :venue_id])
-    |> Ecto.Changeset.validate_required([:venue_id, :artist])
+    |> Ecto.Changeset.cast(params, [:artist, :date, :venue_id, :songkick_id])
+    |> Ecto.Changeset.validate_required([:venue_id, :artist, :songkick_id])
+    |> Ecto.Changeset.unique_constraint(:songkick_id)
   end
 
   def parse(venue_id, %{
+        "id" => songkick_id,
         "performance" => performance,
         "start" => %{"date" => date}
       }) do
@@ -25,6 +28,7 @@ defmodule Upcoming.Event do
 
     %Upcoming.Event{}
     |> Upcoming.Event.changeset(%{
+      songkick_id: to_string(songkick_id),
       artist: artist,
       venue_id: venue_id,
       date:
--- priv/repo/migrations/20191017192338_add_event.exs
+++ priv/repo/migrations/20191017192338_add_event.exs
@@ -6,6 +6,9 @@ defmodule Upcoming.Repo.Migrations.AddEvent do
       add :artist, :string
       add :date, :date
       add :venue_id, references(:venues)
+      add :songkick_id, :string
     end
+
+    create unique_index(:events, [:songkick_id])
   end
 end
