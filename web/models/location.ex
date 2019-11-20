1c7fa8df7196054ad23bffefc2cadc53a0ce9299
Felipe Lacerda <fegolac@gmail.com>
2019-11-20 20:27:33 +0100

    Remove unused files

--- web/models/location.ex
+++ web/models/location.ex
@@ -19,18 +19,6 @@ defmodule Upcoming.Location do
     |> Ecto.Changeset.unique_constraint(:songkick_id)
   end
 
-  def events(location) do
-    from(l in Upcoming.Location,
-      where: l.id == ^location.id,
-      left_join: venues in assoc(l, :venues),
-      left_join: events in assoc(venues, :events),
-      preload: [venues: {venues, events: events}]
-    )
-    |> Upcoming.Repo.one()
-    |> Map.get(:venues)
-    |> Enum.flat_map(fn %Upcoming.Venue{events: events} -> events end)
-  end
-
   def venues_with_events(location) do
     Upcoming.Repo.all(
       from v in Upcoming.Venue, where: ^location.id == v.location_id, preload: [:events]
--- web/models/venue.ex
+++ web/models/venue.ex
@@ -14,43 +14,4 @@ defmodule Upcoming.Venue do
     |> Ecto.Changeset.validate_required([:songkick_id, :name, :location_id])
     |> Ecto.Changeset.unique_constraint(:songkick_id)
   end
-
-  def parse(%{"id" => id, "displayName" => name}) do
-    %Upcoming.Venue{id: id, name: name}
-  end
-
-  def get_from_file(filename) do
-    Upcoming.Songkick.fetch_venues_from_file(filename) |> Enum.map(&parse/1)
-  end
-
-  def get_from_location(location, page \\ 1, per_page \\ 50) do
-    %{results: results, max: max} =
-      Upcoming.Songkick.fetch_venues_from_location(location, page, per_page)
-
-    case results do
-      %{"venue" => venues} ->
-        parsed_venues = Enum.map(venues, &parse/1)
-
-        if (page - 1) * length(venues) >= max do
-          parsed_venues
-        else
-          parsed_venues ++ get_from_location(location, page + 1, per_page)
-        end
-
-      _ ->
-        []
-    end
-  end
-
-  def get_calendar(venue) do
-    import Ecto.Query, only: [from: 2]
-
-    Upcoming.Repo.all(from(e in Upcoming.Event, where: e.venue_id == ^venue.id))
-  end
-
-  def fetch_calendar(venue) do
-    Upcoming.Songkick.fetch_venue_calendar(venue.songkick_id)
-    |> Enum.map(fn e -> Upcoming.Event.parse(e).event end)
-    |> Enum.map(&Upcoming.Repo.insert/1)
-  end
 end
